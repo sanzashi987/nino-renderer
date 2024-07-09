3a3e1571c6506f5b05d5d4890071f66edddb6ecd
cza14h <ziao.chen1995@gmail.com>
2024-07-09 18:35:23 +0800

    feat: add vec cross product

--- tinyrenderer/src/math/vec.rs
+++ tinyrenderer/src/math/vec.rs
@@ -25,6 +25,33 @@ macro_rules! define_vec {
         }
       }
 
+      pub fn dot(&self, rhs:&Self) -> f32{
+        $(
+          self.$p * rhs.$p +
+        )+
+        0.0
+      }
+
+      pub fn length_square(&self) -> f32 {
+        $(
+          self.$p * self.$p +
+        )+
+        0.0
+      }
+
+      pub fn length(&self) -> f32 {
+        self.length_square().sqrt()
+      }
+
+      pub fn normalize(&self) -> Self {
+        let length = self.length();
+        Self {
+          $(
+            $p: self.$p/length,
+          )+
+        }
+      }
+
     }
   };
 }
@@ -32,3 +59,18 @@ macro_rules! define_vec {
 define_vec!(Vec2, x, y);
 define_vec!(Vec3, x, y, z);
 define_vec!(Vec4, x, y, z, w);
+
+impl Vec2 {
+  pub fn cross(&self, rhs: &Self) -> f32 {
+    self.x * rhs.y - self.y * rhs.x
+  }
+}
+impl Vec3 {
+  pub fn cross(&self, rhs: &Self) -> Self {
+    Self {
+      x: self.y * rhs.z - self.z * rhs.y,
+      y: self.z * rhs.x - self.x * rhs.z,
+      z: self.x * rhs.y - self.y * rhs.x,
+    }
+  }
+}
