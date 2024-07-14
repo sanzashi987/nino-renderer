cffaee4662d95b45d0580d6a8ac0ea8e2f4e0391
cza14h <ziao.chen1995@gmail.com>
2024-07-15 01:32:34 +0800

    feat: add mtlparser init

--- tinyrenderer/src/obj_loader/mtl_parser.rs
+++ tinyrenderer/src/obj_loader/mtl_parser.rs
@@ -2,8 +2,45 @@
 use super::{
   file_loader::FileLoader,
   model::{Model, Scene, VertexPointer},
+  obj_parser::ParserMode,
+  ParserError,
 };
 
 pub struct MtlParser<'a> {
   loader: FileLoader<'a>,
+  lazy: bool,
+}
+
+impl<'a> MtlParser<'a> {
+  pub fn new(filepath: &std::path::Path, mode: ParserMode) -> Result<Self, ParserError> {
+    let parent = filepath.parent();
+    let filename = filepath.file_name();
+    let loader_result: Result<FileLoader<'a>, std::io::Error> = FileLoader::new(filepath);
+
+    let (lazy, single) = match mode {
+      ParserMode::Standard => (false, true),
+      ParserMode::Lazy => (true, false),
+    };
+
+    if let (Some(dir), Some(name)) = (parent, filename) {
+      match loader_result {
+        Ok(loader) => {
+          let mut this = Self {
+            loader: loader,
+            lazy,
+          };
+
+          if !lazy {
+            this.parse()?
+          }
+
+          return Ok(this);
+        }
+        Err(e) => return Err(ParserError::IoError(e)),
+      }
+    };
+    Err(ParserError::NotAValidPath)
+  }
+
+  pub fn parse(&self) {}
 }
