a98000e16d53e592552520c8ac91f7c9a49424cf
cza14h <ziao.chen1995@gmail.com>
2024-07-16 19:40:24 +0800

    feat: add mtl parser

--- tinyrenderer/src/obj_loader/defines.rs
+++ tinyrenderer/src/obj_loader/defines.rs
@@ -9,9 +9,67 @@ pub enum ParserError {
   CantConvertToType,
   UnExpectedEndOfLine,
   ModelNotInit,
+  MaterialNotFound,
 }
 
 pub enum ParserMode {
   Lazy,
   None,
 }
+
+macro_rules! parse_num {
+  ($var:ident,$type:ty) => {{
+    $var
+      .parse::<$type>()
+      .map_err(|_| ParserError::CantConvertToNum)?
+  }};
+  ($exp:expr,$type:ty) => {{
+    let val = $exp;
+    val
+      .parse::<$type>()
+      .map_err(|_| ParserError::CantConvertToNum)?
+  }};
+}
+
+macro_rules! parse_token {
+    ($iter: expr; $type:ty) => {{
+      let result = if let Some(s) = $iter {
+        s
+          .parse::<$type>()
+          .map_err(|_| ParserError::CantConvertToType)
+      } else {
+        Err(ParserError::ParseIncomplete("not a valiad string".to_string()))
+      };
+      // move to next token
+      result
+    }};
+
+    ($iter: expr; $type:ty = $($attr:ident : $attr_type:ty),+) => {
+      {
+        let mut val = <$type>::zero();
+        $(
+          if let Some(s) = $iter {
+            val.$attr = parse_num!(s, $attr_type);
+          } else {
+            return Err(ParserError::UnExpectedEndOfLine)
+          }
+        )+
+
+        Ok::<$type,ParserError>(val)
+      }
+    };
+}
+
+// only convert parse token's Result to Option
+macro_rules! parse_token_ok {
+  ($iter: expr; $type:ty) => {
+    parse_token!($iter; $type).ok()
+  };
+  ($iter: expr; $type:ty = $($attr:ident : $attr_type:ty),+) => {
+    parse_token!($iter; $type = $($attr: $attr_type),+).ok()
+  };
+}
+
+pub(super) use parse_num;
+pub(super) use parse_token;
+pub(super) use parse_token_ok;
--- tinyrenderer/src/obj_loader/parser.rs
+++ tinyrenderer/src/obj_loader/parser.rs
@@ -1,32 +1,27 @@
-use super::{
-  defines::{self, ParserError},
-  model::Face,
-};
-use crate::math::{Vec2, Vec3};
-use std::{borrow::BorrowMut, marker::PhantomData, ops::Not, path::Path};
+use super::defines::{self, ParserError};
+use std::{marker::PhantomData, path::Path};
 
-use super::{
-  file_loader::FileLoader,
-  model::{Model, Scene, VertexPointer},
-};
-
-pub trait ParseLine {
-  fn parse_line(&mut self, s: &str) -> Result<(), ParserError>;
+use super::file_loader::FileLoader;
+pub trait ParseLine<Data: Default> {
+  fn parse_line(
+    data: &mut Data,
+    tokens: &mut std::str::SplitWhitespace,
+    s: &str,
+  ) -> Result<(), ParserError>;
 }
 
-pub struct Parser<'a, 'b, Data: Default, Abstracts: ParseLine> {
+pub struct Parser<'a, 'b, Data: Default, Abstracts: ParseLine<Data>> {
   data: Data,
   filepath: &'a Path,
   loader: Option<FileLoader<'b>>,
-  lazy: bool,
+  // lazy: bool,
   _phantom: PhantomData<Abstracts>,
 }
-
 impl<'a, 'b, Data, Abstracts> Parser<'a, 'b, Data, Abstracts>
 where
   'a: 'b,
   Data: Default,
-  Abstracts: ParseLine,
+  Abstracts: ParseLine<Data>,
 {
   pub fn new(filepath: &'a Path, mode: defines::ParserMode) -> Result<Self, ParserError> {
     let lazy = match mode {
@@ -38,7 +33,6 @@ where
       filepath,
       loader: None,
       data: Default::default(),
-      lazy,
       _phantom: PhantomData,
     };
 
@@ -68,84 +62,7 @@ where
 
       let token = tokens.next();
       if let Some(s) = token {
-        match s {
-          "#" => {
-            continue;
-          }
-          "g" | "o" => {
-            let name = parse_token!(tokens.next(); String)?;
-            self.scene.add_model(name);
-          }
-          "v" => {
-            self
-              .scene
-              .add_vertex(parse_token!(tokens.next(), Vec3 = x:f32, y:f32, z:f32)?);
-          }
-          "vn" => {
-            self
-              .scene
-              .add_normal(parse_token!(tokens.next(), Vec3 = x:f32, y:f32, z:f32)?);
-          }
-          "vt" => {
-            self
-              .scene
-              .add_texture_coordinate(parse_token!(tokens.next(), Vec2 = x:f32, y:f32)?);
-          }
-          "f" => {
-            // let mut vertex_pointer = VertexPointer {};
-            let mut vertex_vec = vec![];
-            let mut done = false;
-
-            while !done {
-              match tokens.next() {
-                Some(str) => {
-                  let splited: Vec<&str> = str.split("/").collect();
-                  let indices = splited.as_slice();
-                  if indices.len() > 3 || indices.len() < 1 {
-                    return Err(ParserError::InvalidSyntax(
-                      "face vertex indices".to_string(),
-                    ));
-                  }
-
-                  let (mut texture_index, mut normal_index) = (None, None);
-
-                  match *indices {
-                    [_, second, third] => {
-                      if second.is_empty().not() {
-                        texture_index = Some(parse_num!(second, u32) - 1);
-                      }
-                      normal_index = Some(parse_num!(third, u32) - 1)
-                    }
-                    [_, second] => {
-                      texture_index = Some(parse_num!(second, u32) - 1);
-                    }
-                    _ => return Err(ParserError::InvalidSyntax("face vertex format".to_string())),
-                  }
-                  // let str = indices[0];
-                  let vertex_index = parse_num!(indices[0], u32) - 1;
-                  vertex_vec.push(VertexPointer::new(
-                    vertex_index,
-                    normal_index,
-                    texture_index,
-                  ));
-                }
-                None => {
-                  done = true;
-                }
-              }
-            }
-            if vertex_vec.len() != 3 {
-              // return Err(ParserError::)
-            }
-
-            let vertices: [VertexPointer; 3] = [vertex_vec[0], vertex_vec[1], vertex_vec[2]];
-            self.scene.add_face(Face { vertices })?;
-          }
-          _ => {
-            continue;
-            // _ => return Err(ParserError::UnknownToken(s.to_string())),
-          }
-        }
+        Abstracts::parse_line(&mut self.data, &mut tokens, s)?;
       }
     }
 
@@ -157,5 +74,13 @@ where
     self.parse()?;
 
     Ok(&mut self.data)
+  }
+
+  pub fn get_data_mut(&mut self) -> &mut Data {
+    &mut self.data
+  }
+
+  pub fn get_data_own(self) -> Data {
+    self.data
   }
 }
