a8ffc7509ea9db55e3349bd08abe9d5fdfc7c9a2
cza14h <ziao.chen1995@gmail.com>
2024-08-02 23:52:16 +0800

    feat: add z=n projection martrix

--- tinyrenderer/src/renderer/camera.rs
+++ tinyrenderer/src/renderer/camera.rs
@@ -20,12 +20,23 @@ impl Frustum {
       far,
       aspect,
       fov,
-      mat: Mat4::from_row(&[
-            near / half_w,           0.0,                       0.0,                               0.0,
-                      0.0, near / half_h,                       0.0,                               0.0,
-                      0.0,           0.0, (far + near) / (near - far), 2.0 * far * near / (near - far),
-                      0.0,           0.0,                      -1.0,                               0.0,
-          ])
+      mat: if cfg!(feature="cpu") {
+        let a = 1.0 / (near * fov.tan());
+        // without far plane, clamp x,y in [-1, 1], z = near
+        Mat4::from_row(&[
+          a,          0.0,         0.0, 0.0,
+          0.0, aspect * a,         0.0, 0.0,
+          0.0,        0.0,         1.0, 0.0,
+          0.0,        0.0, -1.0 / near, 0.0,
+        ])
+      } else {
+        Mat4::from_row(&[
+          near / half_w,           0.0,                       0.0,                               0.0,
+          0.0, near / half_h,                       0.0,                               0.0,
+          0.0,           0.0, (far + near) / (near - far), 2.0 * far * near / (near - far),
+          0.0,           0.0,                      -1.0,                               0.0,
+        ])
+      }
     }
   }
 
