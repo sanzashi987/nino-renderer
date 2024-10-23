77a765e608a02ae1003248f9750353b28d397d50
cza14h <ziao.chen1995@gmail.com>
2024-10-24 01:32:16 +0800

    feat: add perspective camera

--- three/src/cameras/camera.rs
+++ three/src/cameras/camera.rs
@@ -1,7 +1,15 @@
-use crate::math::Mat4;
+use crate::{core::object_3d::ObjectActions, math::Mat4};
 
-pub trait Camera {
+pub trait Camera: ObjectActions {
   fn view_matrix(&self) -> Mat4;
   fn projection_matrix(&self) -> Mat4;
-  fn global_matrix_inverse(&self) -> Mat4;
+
+  fn project_matrix_inverse(&self) -> Mat4 {
+    self.projection_matrix().inverse().unwrap()
+  }
+
+  fn global_matrix_inverse(&self) -> Mat4 {
+    self.update_global_matrix();
+    self.global_matrix().inverse().unwrap()
+  }
 }
--- three/src/cameras/perspective_camera.rs
+++ three/src/cameras/perspective_camera.rs
@@ -1,1 +1,67 @@
-pub struct PerspectiveCamera {}
+use std::rc::Rc;
+
+use renderer_macro_derive::object_3d;
+
+use crate::{
+  core::object_3d::{with_default_fields, ObjectActions},
+  math::Mat4,
+};
+
+use super::camera::Camera;
+
+#[object_3d(ObjectActions)]
+pub struct PerspectiveCamera {
+  // in degree, stands for angle from top to bottom
+  pub fov: f32,
+  pub aspect: f32,
+  pub near: f32,
+  pub far: f32,
+  pub focus: f32,
+  pub zoom: f32,
+}
+
+impl PerspectiveCamera {
+  pub fn new(fov: f32, aspect: f32, near: f32, far: f32) -> Rc<Self> {
+    let (focus, zoom) = (10.0, 1.0);
+
+    with_default_fields!(Camera;fov,aspect,near ,far,focus,zoom)
+  }
+
+
+  #[rustfmt::skip]
+  pub fn make_perspective_mat(&self) -> Mat4 {
+    let top = self.near * (self.fov.to_radians() / 2.0).tan() / self.zoom;
+    let height = top * 2.0;
+    let width = self.aspect * height;
+    let left = -0.5 * width;
+    let right = left + width;
+    let bottom = top - height;
+
+    let x = 2.0 * self.near / (right - left);
+    let y = 2.0 * self.near / (top - bottom);
+
+    let a = (right + left) / (right - left);
+    let b = (top + bottom) / (top - bottom);
+
+    let base = self.near - self.far;
+    let c = (self.far + self.near) / base;
+    let d = 2.0 * self.far * self.near / base;
+
+    Mat4::from_row(&[
+        x,  0.0,    a,  0.0,
+      0.0,    y,    b,  0.0,
+      0.0,  0.0,    c,    d,
+      0.0,  0.0, -1.0,  0.0,
+    ])
+  }
+}
+
+impl Camera for PerspectiveCamera {
+  fn view_matrix(&self) -> crate::math::Mat4 {
+    self.global_matrix_inverse()
+  }
+
+  fn projection_matrix(&self) -> crate::math::Mat4 {
+    self.make_perspective_mat()
+  }
+}
