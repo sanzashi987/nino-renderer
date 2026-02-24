baff87df0b001d9ed78e8f697805261e6dd29b6a
sanzashi987 <chen_ziao@126.com>
2026-02-24 19:20:49 +0800

    feat(sphere): add constructor and ray intersection method

--- tinytracer/src/object/sphere.rs
+++ tinytracer/src/object/sphere.rs
@@ -4,3 +4,13 @@ pub struct Sphere {
   pub center: Vec3,
   pub radius: f32,
 }
+
+impl Sphere {
+  pub fn new(center: Vec3, radius: f32) -> Self {
+    Self { center, radius }
+  }
+
+  pub fn ray_intersect(&self, ray_orig: &Vec3) -> bool {
+
+  }
+}
