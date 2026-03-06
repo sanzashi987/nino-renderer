236269d8c66152f33e88fd6be4800e03073fa4f9
cza14h <ziao.chen1995@gmail.com>
2026-03-07 04:41:12 +0800

    feat: reorganize math module by adding new modules and removing obsolete file

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -1,9 +1,15 @@
 mod barycentric;
-mod vec;
 mod mat;
+mod vec;
 pub use self::mat::*;
 pub use self::vec::*;
 pub use barycentric::Barycentric;
 mod boundary_box;
 pub use self::boundary_box::BoundaryBox;
+pub mod data_array;
+pub mod euler;
 pub mod frustum;
+mod quaternion;
+pub use quaternion::*;
+mod rotate;
+pub use rotate::*;
