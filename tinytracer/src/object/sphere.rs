6b05373d0b9c4c71df4b0b939716f368532bd7fb
cza14h <ziao.chen1995@gmail.com>
2026-03-07 02:39:25 +0800

    feat(sphere): refactor ray intersection method to return distance as Option<f32>

--- tinytracer/src/object/sphere.rs
+++ tinytracer/src/object/sphere.rs
@@ -10,35 +10,33 @@ impl Sphere {
     Self { center, radius }
   }
 
-  pub fn ray_intersect(
-    &self,
-    ray_origin: &Vec3,
-    ray_dir: &Vec3,
-    first_intersect: &mut f32,
-  ) -> bool {
+  /**
+   * @return the distance to the ray origin when first intersection occurs
+   */
+  pub fn ray_intersect(&self, ray_origin: &Vec3, ray_dir: &Vec3) -> Option<f32> {
     let to_center = self.center - *ray_origin;
     let to_center_projected_at_dir = to_center * *ray_dir; // dot product
     let ray_to_center =
       to_center * to_center - to_center_projected_at_dir * to_center_projected_at_dir;
     let raduis_exq = self.radius.exp2();
     if ray_to_center > raduis_exq {
-      return false;
+      return None;
     }
 
     let half_chord = (raduis_exq - ray_to_center).sqrt();
 
-    *first_intersect = to_center_projected_at_dir - half_chord;
+    let mut first_intersect = to_center_projected_at_dir - half_chord;
     let far_intersect = to_center_projected_at_dir + half_chord;
 
-    if *first_intersect < 0.0 {
+    if first_intersect < 0.0 {
       // emit inside the sphere
-      *first_intersect = far_intersect;
+      first_intersect = far_intersect;
     }
 
-    if *first_intersect < 0.0 {
-      return false;
+    if first_intersect < 0.0 {
+      return None;
     }
 
-    return true;
+    return Some(first_intersect);
   }
 }
